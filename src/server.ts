513289854a801aa2f3ac3980e516c16da338467f
thatipradeepreddy <pradeep.thati@qylis.com>
2025-11-30 15:16:09 +0530

    made chnges

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,16 @@ app.use(
 	})
 )
 
-app.options("/*", cors())
+app.use((req, res, next) => {
+	if (req.method === "OPTIONS") {
+		res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN)
+		res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		res.header("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		res.header("Access-Control-Allow-Credentials", "true")
+		return res.sendStatus(204)
+	}
+	return next()
+})
 
 app.use("/auth", authRoutes)
 app.use("/s3", s3Routes)
